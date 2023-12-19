949ff0fdc42000eaf7e7d4a0f9feb6bd713df7be
Jigar Patel <jigar1822@gmail.com>
2023-12-19 03:45:48 -0500

    Complete image upload & intiate display

--- transformpix/src/App.js
+++ transformpix/src/App.js
@@ -8,6 +8,7 @@ import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+
 import { Button, Card, Typography } from '@mui/material';
 
 import HeaderBar from './components/HeaderBar';
@@ -55,38 +56,85 @@ const VisuallyHiddenInput = styled('input')({
 
 function App() {
   const [isdarkMode, setIsDarkMode] = React.useState(false);
+  const [uploadedImages, setUploadedImages] = React.useState([]);
+  const imageInputRef = React.useRef(null);
+
 
   const handleDarkModeToggle = (value) => {
     setIsDarkMode(value);
   }
 
+  const handleUploadClick = (event) => {
+    imageInputRef.current.click();
+  }
+
+  const handleImageInput = (event) => {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        selectedFile.imageData = reader.result;
+        const newImages = [...uploadedImages, selectedFile];
+        setUploadedImages(newImages);
+      };
+      reader.readAsDataURL(selectedFile)
+    }
+  }
 
   return (
     <ThemeProvider theme={isdarkMode ? darkTheme : lightTheme}>
       <HeaderBar onValueChange={handleDarkModeToggle}></HeaderBar>
       <Container maxWidth="sm" >
         <Stack mt={5} spacing={5} justifyContent="center" alignItems="center">
-          <Box sx={{ minHeight: '65vh', display: 'flex', alignItems: 'center' }}>
-            <Typography variant='h4' sx={{ fontWeight: '500' }}>
-              {noImagesEmoji}
-            </Typography>
-          </Box>
-          <Button component="label" variant='contained' elevation={3}>
+          {uploadedImages.length === 0 ? (
+            <Box sx={{ minHeight: '65vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+              <Typography variant='h4' sx={{ fontWeight: '600' }} mb={3}>
+                No Images
+              </Typography>
+              <Typography variant='h4' sx={{ fontWeight: '200' }}>
+                {noImagesEmoji}
+              </Typography>
+            </Box>
+          ) : (
+            <div style={{ height: '65vh', overflowY: 'auto' }}>
+              <Stack spacing={3} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                {uploadedImages.map((image, index) => {
+                  console.log(image);
+                  return (
+                    <Card sx={{position: 'relative'}}>
+                      <img src={image.imageData} key={index}></img>
+                      <div className="image-overlay">
+                        <div className="image-dimensions">
+                          {image.width}px x {image.height}px
+                        </div>
+                      </div>
+                    </Card>
+                  )
+                })}
+              </Stack>
+            </div>
+          )}
+        </Stack>
+        <Box mt={5} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          <Button component="label" variant='contained' elevation={3} onClick={handleUploadClick}>
             <CloudUpload sx={{ fontSize: 60, paddingRight: 2 }} color='secondary' />
-            <Typography variant='h5' sx={{ fontWeight: 'bolder' }}>
+            <Typography variant='h5' sx={{ fontWeight: '600' }}>
               Upload image
             </Typography>
-            <VisuallyHiddenInput type="file" sx={{ display: 'none' }} accept="image/png, image/jpeg"/>
+            <VisuallyHiddenInput ref={imageInputRef} type="file" sx={{ display: 'none' }} onChange={handleImageInput} accept="image/jpg, image/jpeg, image/png" />
           </Button>
-        </Stack>
-
+          <Typography variant='body2' py={1}>
+            No Image leaves this device.
+          </Typography>
+        </Box>
       </Container>
 
       <AppBar position="fixed" style={{ top: 'auto', bottom: 0 }}>
         <Toolbar>
         </Toolbar>
       </AppBar>
-    </ThemeProvider>
+    </ThemeProvider >
   );
 }
 
